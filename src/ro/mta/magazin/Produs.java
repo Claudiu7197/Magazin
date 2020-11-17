c812cc94821a00f2f622babecb35eaba24b5ff81
Bogdan849 <ilie.bogdan.constantin2@gmail.com>
2020-11-18 00:03:15 +0200

    Adaugare produs nou, respectiv update clasa Produs
    Signed-off-by: Bogdan849 <ilie.bogdan.constantin2@gmail.com>

--- src/ro/mta/magazin/Produs.java
+++ src/ro/mta/magazin/Produs.java
@@ -28,6 +28,11 @@ public class Produs implements Comparable<Produs>{
         return pret;
     }
 
+    public String getNume()
+    {
+        return nume;
+    }
+
     @Override
     public String toString()
     {
